5668907c541fb421d7cb97024d5390e87e136096
aronei44 <meliodasmeliodas224@gmail.com>
2025-04-07 13:38:58 +0700

    json

--- src/handlers/hello.rs
+++ src/handlers/hello.rs
@@ -1,9 +1,19 @@
-use axum::extract::Path;
+use axum::{
+    extract::Path,
+    response::Json
+};
+use serde_json::{Value, json};
 
-pub async fn hello_world() -> &'static str {
-    "Hello, World!"
+pub async fn hello_world() -> Json<Value> {
+    let response = json!({
+        "message": "Hello, World!"
+    });
+    Json(response)
 }
 
-pub async fn hello_world_with_id(Path(id): Path<u32>) -> String {
-    format!("Hello, World! with id: {}", id)
+pub async fn hello_world_with_id(Path(id): Path<u32>) -> Json<Value> {
+    let response = json!({
+        "message": format!("Hello, World! with id: {}", id)
+    });
+    Json(response)
 }
