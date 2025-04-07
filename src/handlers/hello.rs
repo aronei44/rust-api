f04a57d838c61ad7bb4d5a81fb27e9def676a3f2
aronei44 <meliodasmeliodas224@gmail.com>
2025-04-07 13:56:46 +0700

    swagger

--- src/handlers/hello.rs
+++ src/handlers/hello.rs
@@ -2,8 +2,16 @@ use axum::{
     extract::Path,
     response::Json
 };
+
 use serde_json::{Value, json};
 
+#[utoipa::path(
+    get,
+    path = "/api/hello",
+    responses(
+        (status = 200, description = "Say hello")
+    )
+)]
 pub async fn hello_world() -> Json<Value> {
     let response = json!({
         "message": "Hello, World!"
@@ -11,6 +19,16 @@ pub async fn hello_world() -> Json<Value> {
     Json(response)
 }
 
+#[utoipa::path(
+    get,
+    path = "/api/hello/{id}",
+    params(
+        ("id" = String, Path, description = "ID of the user")
+    ),
+    responses(
+        (status = 200, description = "Say hello with ID")
+    )
+)]
 pub async fn hello_world_with_id(Path(id): Path<u32>) -> Json<Value> {
     let response = json!({
         "message": format!("Hello, World! with id: {}", id)
--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,33 @@ use axum::{
     Router,
     response::IntoResponse,
 };
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
+
 mod router;
 mod handlers;
 use router::hello_router::create_app;
 
+#[derive(OpenApi)]
+#[openapi(paths(
+    crate::handlers::hello::hello_world,
+    crate::handlers::hello::hello_world_with_id
+))]
+struct ApiDoc;
+
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
+    // Generate Swagger UI
+    let swagger_ui = SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi());
+
+    // Build our application
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .nest("/api", Router::new()
-            .merge(create_app())
-        )
+        .nest("/api", Router::new().merge(create_app()))
+        .merge(swagger_ui) // Add Swagger UI
         .fallback(fallback_handler);
 
-    // run our app with hyper, listening globally on port 3000
+    // Run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
